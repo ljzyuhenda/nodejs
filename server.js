f86075cfb3042f80f112fcb46861382168e717a6
ljzyuhenda <624789028@qq.com>
2016-06-15 14:31:17 +0800

    server

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+function start(route) {
+    function onRequest(request, response) {
+        var pathName = url.parse(request.url).pathname;
+        console.log('pathName:' + pathName + ',url:' + request.url);
+
+        route(pathName);
+
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write('hello world');
+        response.end();
+    };
+
+    http.createServer(onRequest).listen(8888);
+
+    console.log('server has started');
+};
+
+exports.start = start;
