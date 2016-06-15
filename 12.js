8089fa3dcb388ef45581ba762813dc3b0b8cebbb
ljzyuhenda <624789028@qq.com>
2016-06-15 21:10:18 +0800

    express

---
+++ 12.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (request, response) {
+    console.log('get请求');
+
+    response.send('hello world get 12.js');
+});
+
+app.get('/list_user', function (resquest, response) {
+    response.send('list_user');
+});
+
+app.get('/ab*cd', function (request, response) {
+    response.send('正则匹配');
+});
+
+app.post('/', function (request, response) {
+    console.log('post请求');
+
+    response.send('hello world  post 12.js')
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('host:' + host + ",port:" + port);
+});
